676995e5fb2b27d4add550cd02f299328cd31377
samich <itssami@gmail.com>
2023-06-11 23:59:43 -0400

    validation

--- AspNetCoreFormValidation1/Models/Student.cs
+++ AspNetCoreFormValidation1/Models/Student.cs
@@ -1,11 +1,20 @@
-﻿namespace AspNetCoreFormValidation1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreFormValidation1.Models
 {
     public class Student
     {
 
+        //default error message
+        [Required]
         public string Name { get; set; } = string.Empty;
-        public string Email { get; set; }
-        public int Age { get; set; }
+
+        //custom error message
+        [Required(ErrorMessage = "Please enter email address.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Enter a valid number.")]
+        public int? Age { get; set; }
         
 
     }
